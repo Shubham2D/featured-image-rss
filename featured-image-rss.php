4dc14910c78678255088743e33f6d5b5d46517e8
Shubham Sawarkar <shubhamsawarkar@live.com>
2025-03-31 00:37:03 -0400

    Add files via upload

---
+++ featured-image-rss.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Featured Image in RSS Feed
+ * Description: Adds featured images to WordPress RSS feeds.
+ * Version: 1.0
+ * Author: Your Name
+ * License: MIT
+ */
+
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
+function add_featured_image_to_rss($content) {
+    if (is_feed() && has_post_thumbnail()) {
+        $image = get_the_post_thumbnail(get_the_ID(), 'full', array('style' => 'max-width:100%; height:auto;'));
+        $content = $image . $content;
+    }
+    return $content;
+}
+add_filter('the_excerpt_rss', 'add_featured_image_to_rss');
+add_filter('the_content_feed', 'add_featured_image_to_rss');
